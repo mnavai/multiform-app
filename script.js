53e531f7ce7bf9687dbaf8a55f0c76c24d317a33
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-17 22:38:02 -0400

    fixing form validation.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const form = document.getElementById('form');
-form.addEventListener('submit',validateForm);
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('form');
+    form.addEventListener('submit',validateForm);
 
-function validateForm(event){
-    event.preventDefault();
+    function validateForm(event){
+        event.preventDefault();
 
-    //Ensure all fields are filled before submitting the form
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phonenumber = document.getElementById('phonenumber').value;
+        //Ensure all fields are filled before submitting the form
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phonenumber = document.getElementById('phonenumber').value;
 
-    if (name === '' || email === '' || phonenumber === ''){
-        alert("Please fill all fields");
-        return;
+        if (name === '' || email === '' || phonenumber === ''){
+            alert("This field is required");
+            return;
+        }
+
+        form.submit()
     }
-
-    form.submit()
-}
+});
