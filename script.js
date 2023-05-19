3792f0cbaef9798a6d5c78a052f913fc1b61adfc
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-19 19:37:04 -0400

    script file updated with clearing the form after submission

--- script.js
+++ script.js
@@ -20,17 +20,23 @@ document.addEventListener('DOMContentLoaded', function(){
             const error = document.querySelector('.error-text-phone');
             error.style.display = "block";
         }
+
+        //clear the error message on focus
         name.addEventListener('focus', ()=> {
             const errName = document.querySelector('.error-text-name');
             errName.style.display = "none";
         });
-        email.addEventListener('keypress', () => {
-            const errEmail = document.querySelector('.error-text-name');
+        email.addEventListener('focus', () => {
+            const errEmail = document.querySelector('.error-text-email');
             errEmail.style.display = "none";
         });
-        phonenumber.addEventListener('keypress',() => {
-            const errPhone = document.querySelector('.error-text-name');
+        phonenumber.addEventListener('focus',() => {
+            const errPhone = document.querySelector('.error-text-phone');
             errPhone.style.display = "none";
         });
+        //clear the form if successfully submitted
+        if(name.value !== '' && email.value !== '' && phonenumber.value !== ''){
+            form.reset();
+        }
     });
 });
