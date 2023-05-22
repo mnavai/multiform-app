0e801522867f14e22048b400820a07a938c34c89
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-22 15:19:02 -0400

    2nd page finalizing

--- secondpage.js
+++ secondpage.js
@@ -1,15 +1,24 @@
-// Get all the card elements
-const cards = document.querySelectorAll('.card-group .card');
+document.addEventListener('DOMContentLoaded', function(){
 
-// Add a click event listener to each card
-cards.forEach(card => {
-  card.addEventListener('click', function() {
-    // Remove the 'selected' class from all cards
+    // Get all the card elements
+    const cards = document.querySelectorAll('.card-group .card');
+
+    // Add a click event listener to each card
     cards.forEach(card => {
-      card.classList.remove('selected');
+    card.addEventListener('click', function() {
+        // Remove the 'selected' class from all cards
+        cards.forEach(card => {
+        card.classList.remove('selected');
+        });
+
+        // Add the 'selected' class to the clicked card
+        this.classList.add('selected');
+        });
     });
 
-    // Add the 'selected' class to the clicked card
-    this.classList.add('selected');
-  });
+    const secondform = document.getElementById('form');
+    secondform.addEventListener('submit',(e) => {
+        e.preventDefault();
+        window.location.href = 'index.html';
+    });
 });
